4503d1722548c5331498ccdc2e331db6fd1132d1
MohamedElmdary <engm5081@gmail.com>
2020-03-19 10:24:38 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+    createContext,
+    useContext,
+    useState,
+    useMemo,
+    useReducer
+} from 'react';
+import './app.css';
+
+interface MessageCtxModel<T> {
+    message: T;
+    setMessage: React.Dispatch<React.SetStateAction<T>> | any;
+}
+
+const MessageContext = createContext<MessageCtxModel<string>>({
+    message: '',
+    setMessage: () => null
+});
+
+const NestedSubApp = () => {
+    const { message, setMessage } = useContext(MessageContext);
+    return (
+        <div>
+            <h1>
+                <p>{message}</p>
+            </h1>
+            <button onClick={() => setMessage(Math.random().toString())}>
+                set new value
+            </button>
+        </div>
+    );
+};
+
+const SubApp = () => {
+    return (
+        <div>
+            <NestedSubApp />
+        </div>
+    );
+};
+
+function reducer(
+    state: { message: string },
+    { type, message }: { type: string; message: string }
+) {
+    switch (type) {
+        case '[set] message':
+            return {
+                message
+            };
+        default:
+            throw new Error('invalid action');
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, {
+        message: 'default works '
+    });
+    // const [message, setMessage] = useState<string>('Message');
+    const messageProvider = useMemo(
+        () => ({
+            message: state.message,
+            setMessage: (message: string) =>
+                dispatch({ type: '[set] message', message })
+        }),
+        [state.message]
+    );
+
+    return (
+        <MessageContext.Provider value={messageProvider}>
+            <SubApp />
+        </MessageContext.Provider>
+    );
 }
 
 export default App;
